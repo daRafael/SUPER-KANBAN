ffba8844cac84131126520c8766802d42a292d0d
Rafael Guerra <rafa99gm@hotmail.com>
2024-05-09 22:51:26 +0100

    about-page done

--- src/AboutPage.jsx
+++ src/AboutPage.jsx
@@ -5,87 +5,66 @@ export default function AboutPage() {
   return (
     <div className="background">
       <div className="about-container">
-        <div className="profiles-container">
-
-          <div className="profile-container">
-            <div className="profile-img-container">
-              <img className='profile-img' src="https://placehold.co/400" alt="" />
-            </div>
-            <div className="autor-container">
-              <div className="author-name">
-                <h2>Rafael Guerra</h2>
-              </div>
-              <div className="author-description">
-                <div className='logo-container'>
-                  <a href="https://github.com/daRafael" target='/blank'>
-                    <img src="" alt="" className='logo-img' />
-                  </a>
-                  <a href="https://www.linkedin.com/in/rafael-guerra-full-stack-developer/" target='/blank'>
-                    <img src="" alt="" className='logo-img' />
-                  </a>
-                </div>
-                <div>
-                  <p>
-                    IronHack Bootcamp
-                    <br />
-                    <br />
-                    Course: Web Developing
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="profile-container">
-            <div className="profile-img-container">
-              <img className='profile-img' src="https://placehold.co/400" alt="" />
-            </div>
-            <div className="autor-container">
-              <div className="author-name">
-                <h2>Edir Sequeira</h2>
-              </div>
-              <div className="author-description">
-                <div className='logo-container'>
-                  <a href="https://github.com/ejcsequeira" target='/blank'>
-                    <img src="" alt="" className='logo-img' />
-                  </a>
-                  <a href="https://www.linkedin.com/in/edir-sequeira" target='/blank'>
-                    <img src="" alt="" className='logo-img' />
-                  </a>
-                </div>
-                <div>
-                  <p>
-                    IronHack Bootcamp
-                    <br />
-                    <br />
-                    Course: Web Developing
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div className='about-us-title'>
+          About
         </div>
+        <div className='about-us-info-container'>
+          <div className="profiles-container">
 
-        <div className="final-description-container">
-          <div className="final-description">
-            <p>It is a long established fact that a
-              reader will be distracted by the readable
-              content of a page when looking at its layout.
-              The point of using Lorem Ipsum is that it has a
-              more-or-less normal distribution of letters, as
-              opposed to using 'Content here, content here',
-              making it look like readable English.
-              Many desktop publishing packages and web page
-              editors now use Lorem Ipsum as their default model text,
-              and a search for 'lorem ipsum' will uncover many web sites
-              still in their infancy. Various versions have evolved over the years,
-              sometimes by accident, sometimes on purpose
-              (injected humour and the like).</p>
+            <div className="profile-container">
+              <div className="profile-img-container">
+                <img className='profile-img' src="./src/assets/images/rafael-profile.png" alt="" />
+              </div>
+              <div className="author-container">
+                <div className="author-name">
+                  Rafael Guerra
+                </div>
+                <div className="author-links">
+                  <div className='links-container'>
+                    <a href="https://github.com/daRafael" target='/blank'>
+                      <img className='link-logos' src='./src/assets/images/github-logo.png' alt='github-img'/>
+                    </a>
+                    <a href="https://linkedin.com/in/rafael-guerra-full-stack-developer" target='/blank'>
+                      <img className='link-logos' src='./src/assets/images/linkedin-logo.png' alt='linkedin-logo'/>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="profile-container">
+              <div className="profile-img-container">
+                <img className='profile-img' src="./src/assets/images/edir-profile.jpg" alt="" />
+              </div>
+              <div className="author-container">
+                <div className="author-name">
+                  Edir Sequeira
+                </div>
+                <div className="author-links">
+                  <div className='links-container'>
+                    <a href="https://github.com/ejcsequeira" target='/blank'>
+                      <img className='link-logos' src='./src/assets/images/github-logo.png' alt='github-img'/>
+                    </a>
+                    <a href="https://linkedin.com/in/edir-sequeira/" target='/blank'>
+                      <img className='link-logos' src='./src/assets/images/linkedin-logo.png' alt='linkedin-logo'/>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="thank-you">
-            <p>Thank you!</p>
+
+          <div className="about-description-container">
+            <div className="about-description">
+              We are proud to present... SUPER KANBAN!<br />
+              A project marks our journey at IronHack, representing our second project in the bootcamp and our first dive into React. We used dnd-kit, a package used to build a fully fleshed out drag-and-drop feature. It was challenging, but very satisfying when we got it working. That being said, we sincerely hope you enjoy exploring it!
+            </div>
+            <div className='thank-you'>
+              Thank you!
+            </div>
           </div>
-        </div>
+
+        </div>     
       </div>
     </div>
   )
